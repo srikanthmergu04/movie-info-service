0031a720d85f7681846892f0e6696f38fb671281
Srikanth Mergu <srikanth.mergu@pegs.com>
2020-05-02 18:40:14 +0530

    Initial commit

---
+++ src/test/java/com/javatechie/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
